9f8a547e91596398bbfce54a0fa3d98ad4c27e26
dgunther2001 <danielgunther2001@gmail.com>
2024-08-17 23:26:04 -0400

    Add edges and debug print graph

--- include/graph/graph.h
+++ include/graph/graph.h
@@ -6,16 +6,32 @@
 #include <iostream>
 
 template<typename T>
-class vector_entry {
+class adj_list_entry {
     T node_name;
     int weight;
+
+public:
+    adj_list_entry(T node_name, int weight) : 
+            node_name(node_name),
+            weight(weight)
+            {}
+    
+    ~adj_list_entry() {}
+
+    int get_weight() const {
+        return weight;
+    }
+
+    const T get_node() const {
+        return node_name;
+    }
 };
 
 template<typename T>
 class graph {
 private:    
     bool directed;
-    std::map<T, std::vector<vector_entry<T>>> adj_list;
+    std::map<T, std::vector<adj_list_entry<T>>> adj_list;
 
 public:
     graph(bool directed) : directed(directed) {}
@@ -27,9 +43,38 @@ public:
         }
     }
 
+    void print_graph_debug() {
+        for (auto const& [node, adj_list] : adj_list) {
+            std::cout << node << ": ";
+            for(auto const& entry : adj_list) {
+                std:: cout << entry.get_node() << " ";
+            }
+
+            std::cout << "\n";
+        }
+    }
+
     void add_node(T node) {
-        std::vector<vector_entry<T>> empty_adj_list;
+        std::vector<adj_list_entry<T>> empty_adj_list;
         adj_list.insert({node, empty_adj_list});
+    }
+    
+    void add_edge(T node_1, T node_2, int weight) {      
+        if (adj_list.find(node_1) == adj_list.end()) {
+            add_node(node_1);
+        }
+
+        if (adj_list.find(node_2) == adj_list.end()) {
+            add_node(node_2);
+        }
+
+        adj_list_entry<T> primary_entry(node_2, weight);
+        adj_list[node_1].push_back(primary_entry);
+
+        if (!directed) {
+            adj_list_entry<T> secondary_entry(node_1, weight);
+            adj_list[node_2].push_back(secondary_entry);
+        }
     }
 
 };
--- tests/main.cpp
+++ tests/main.cpp
@@ -6,12 +6,16 @@ int main() {
 
     graph<int> my_graph(true);
     my_graph.add_node(1);
-    my_graph.print_nodes_debug();
+    //my_graph.print_nodes_debug();
 
     std::cout << "\n";
 
     my_graph.add_node(17);
-    my_graph.print_nodes_debug();
+    //my_graph.print_nodes_debug();
+
+    my_graph.add_edge(1, 2, 17);
+
+    my_graph.print_graph_debug();
 
     return 0;
 }
